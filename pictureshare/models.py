6586ab5bfd65b8b0f767423d9f086aeb8d291b4f
12moi <12moi>
2022-03-24 21:47:42 +0300

    add Image model

--- pictureshare/models.py
+++ pictureshare/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    Image=models.ImageField(upload_to='')
+    name = models.CharField(max_length=70)
+    description = models.TextField()
+    author = models.CharField(max_length=50, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+    # category = models.ForeignKey(Category)
+    # location = models.ForeignKey(Location)
+
+
+
+class Category(models.Model):
+    location = models
+
+
+class Location(models.Model):
+    location = models
