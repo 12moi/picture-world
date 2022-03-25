74777d5266ad330c0f775e000095f79ec2660306
12moi <12moi>
2022-03-25 15:07:39 +0300

    add updates

--- pictureshare/urls.py
+++ pictureshare/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import path
+from django.conf.urls import re_path
 from . import views
 from  django.conf.urls.static import static
 
 
 urlpatterns=[
-    path(r'^', views.index, name='index',),
+    re_path(r'^', views.index, name='',),
 
 ]
