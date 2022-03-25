31b184367af9d16f4a3a0bd37be346f6a7550ef9
12moi <12moi>
2022-03-25 18:11:22 +0300

    add updates to DATABASE in settings.py file

--- pictureshare/settings.py
+++ pictureshare/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'pictureshare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':  'pictures',
         'USER':'postgres',
         'PASSWORD':'12moringa',
