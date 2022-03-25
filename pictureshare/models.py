489e27f848884098be6ac26b42e6d184985108ca
12moi <12moi>
2022-03-25 08:17:51 +0300

    add category method to models.py

--- pictureshare/models.py
+++ pictureshare/models.py
@@ -1,6 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
 
 class Image(models.Model):
     Image=models.ImageField(upload_to='')
@@ -8,14 +41,6 @@ class Image(models.Model):
     description = models.TextField()
     author = models.CharField(max_length=50, default='admin')
     date = models.DateTimeField(auto_now_add=True)
-    # category = models.ForeignKey(Category)
-    # location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
 
-
-
-class Category(models.Model):
-    location = models
-
-
-class Location(models.Model):
-    location = models
