3bf2aa40a872deefdcd0f43cfe089dc58de5adc8
12moi <12moi>
2022-03-26 13:04:18 +0300

    add search regex to url.py file

--- pictures/urls.py
+++ pictures/urls.py
@@ -8,5 +8,6 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
-    
+    re_path(r'^search/', views.search_results, name='search'),
+    re_path(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
 ]
